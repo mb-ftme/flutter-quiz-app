0f4a5dc267f8986463888f56c1a57fc9daf4514e
f.barzegar <fmuser1999@gmail.com>
2023-01-16 16:24:41 +0330

    create

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Quiz App'),
+        ),
+        body: Text('this is my default text!'),
+      ),
+    );
+  }
+}
