669de398464409ba0db342e0cc61b65f45e8e664
f.barzegar <fmuser1999@gmail.com>
2023-01-17 16:30:29 +0330

    add question and answer

---
+++ lib/answer.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(7),
+      child: ElevatedButton(
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_quiz_app/answer.dart';
+import 'package:my_quiz_app/question.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'whats your fav color?',
+      'answers': ['black', 'red', 'green', 'white'],
+    },
+    {
+      'questionText': 'whats your fav animal?',
+      'answers': ['rabbit', 'snake', 'cat', 'dog']
+    },
+    {
+      'questionText': 'whats your fav food?',
+      'answers': ['pasta', 'pizza', 'soshi', 'dog']
+    },
+  ];
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('we have more');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +49,13 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My Quiz App'),
         ),
-        body: Text('this is my default text!'),
+        body: Column(children: [
+          Question(questions[_questionIndex]['questionText'] as String),
+          ...(questions[_questionIndex]['answers'] as List<String>)
+              .map((answer) {
+            return Answer(_answerQuestion, answer);
+          }).toList()
+        ]),
       ),
     );
   }
